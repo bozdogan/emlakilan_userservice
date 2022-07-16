18675d5bddb07acc9d0b87d18478f283de783040
Bozdogan <boraozdogan99@gmail.com>
2022-07-16 12:17:52 +0300

    add tests

--- src/test/java/org/bozdgn/userservice/service/UserServiceTest.java
+++ src/test/java/org/bozdgn/userservice/service/UserServiceTest.java
@@ -1,22 +1,26 @@
 package org.bozdgn.userservice.service;
 
+import org.bozdgn.userservice.model.User;
 import org.bozdgn.userservice.repository.UserRepository;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
 class UserServiceTest {
 
     private static PasswordEncoder passwordEncoder;
     @Mock
     private UserRepository userRepository;
-    private AutoCloseable mocks;
     private UserService underTest;
 
     @BeforeAll
@@ -26,7 +30,6 @@ class UserServiceTest {
 
     @BeforeEach
     void setUp() {
-        mocks = MockitoAnnotations.openMocks(this);
         underTest = new UserService(userRepository, passwordEncoder);
     }
 
@@ -36,8 +39,24 @@ class UserServiceTest {
         Mockito.verify(userRepository).findAll();  // NOTE(bora): This test passes if UserRepository#findAll method is invoked.
     }
 
-    @AfterEach
-    void tearDown() throws Exception {
-        mocks.close();
+    @Test
+    void canSaveUser() {
+        User user = new User(
+                null,
+                "bjork",
+                "test",
+                "bjork@bjorkss.on",
+                false,
+                "Björk",
+                "Björkoğlu",
+                "01112223344");
+
+        underTest.saveEntity(user);
+
+        ArgumentCaptor<User> userArgumentCaptor = ArgumentCaptor.forClass(User.class);
+        Mockito.verify(userRepository).save(userArgumentCaptor.capture());
+
+        User capturedUser = userArgumentCaptor.getValue();
+        assertThat(capturedUser).isEqualTo(user);
     }
 }
